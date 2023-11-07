04bc3ac61d03d98f992fcbd4361a1f67cf1ea7ab
Norman Gray <gray@nxg.name>
2023-11-07 15:46:16 +0000

    Fix DOI formatting.
    Rework the parsing of DOIs, so that if the URL field starts with
    "http://doi.org" or "https://doi.org", then we match the corresponding
    DOI in both cases.
    
    Reworked test cases to be more systematic.

--- test/test-ivoa-bst-urls.sh
+++ test/test-ivoa-bst-urls.sh
@@ -2,14 +2,15 @@
 
 name=test-ivoa-bst-urls
 
-BIBINPUTS=.. BSTINPUTS=.. bibtex $name >$name.stdout
-grep -Ei '(harvarditem|doi|url)' $name.bbl >$name.out
+BSTINPUTS=.. bibtex $name >$name.stdout
+#grep -Ei '(harvarditem|doi|url)' $name.bbl >$name.out
+#mv $name.bbl $name.out
 
 status=0
 
-if diff $name.good $name.out >$name.diff; then
+if diff $name.good $name.bbl >$name.diff; then
     echo "$0 good"
-    for e in bbl blg out diff stdout; do rm $name.$e; done
+    for e in bbl blg diff stdout; do rm $name.$e; done
 else
     echo "$0 failed: diffs in $name.diff"
     status=1
